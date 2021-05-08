a4b67da46d85df218f4fae8d227fffe05e89d7ba
Suryadipsinh Vaghela <77860201+Suryadipsinh@users.noreply.github.com>
2021-05-09 03:16:21 +0530

    Create Q5: Liftoff.py

---
+++ Assignment-2: Khansole/Q5: Liftoff.py
@@ -0,0 +1,13 @@
+"""
+Prints out a spaceship launch sequence.
+"""
+
+def main():
+    i=10
+    while i>=1:
+        print(i)
+        i = i-1
+    print("Liftoff!")
+
+if __name__ == '__main__':
+    main()
