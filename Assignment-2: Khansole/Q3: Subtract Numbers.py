be51e23db0ed05da56526ca5d1837fe2e35e986e
Suryadipsinh Vaghela <77860201+Suryadipsinh@users.noreply.github.com>
2021-05-09 03:13:57 +0530

    Create Q3: Subtract Numbers.py

---
+++ Assignment-2: Khansole/Q3: Subtract Numbers.py
@@ -0,0 +1,15 @@
+"""
+Asks the user for two numbers and prints the result
+of the first number minus the second number.
+"""
+
+def main():
+    print("This program subtracts one number from another.")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    sub = num1-num2
+    print("The result is", sub)
+
+
+if __name__ == '__main__':
+    main()
